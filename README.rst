af25f5cbd8bbd5cfff78a1fdc65a28b6b4050f89
Colin Copeland <copelco@caktusgroup.com>
2010-05-26 12:05:14 -0400

    add file_picker app

---
+++ README.rst
@@ -0,0 +1,8 @@
+-*- restructuredtext -*-
+
+django-file-picker
+==================
+
+Pluggable file picker.
+
+Development by `Caktus Consulting Group <http://www.caktusgroup.com/>`_.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+packages = find_packages()
+packages.remove('sample_project')
+setup(
+    name='django-file-picker',
+    version='0.0.0',
+    author='Caktus Consulting Group',
+    author_email='solutions@caktusgroup.com',
+    packages=find_packages(),
+    include_package_data=True,
+    exclude_package_data={
+        '': ['*.sql', '*.pyc'],
+    },
+    url='',
+    license='LICENSE.txt',
+    description='Pluggable file picker',
+    long_description=open('README.rst').read(),
+)
