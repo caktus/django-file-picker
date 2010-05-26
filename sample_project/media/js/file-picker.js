d8fb702341d0a7c3e372029539001199705a477d
glassresistor <none@none>
2010-05-26 15:52:33 -0400

    Initial img insert

--- sample_project/media/js/file-picker.js
+++ sample_project/media/js/file-picker.js
@@ -40,16 +40,55 @@ function FilePicker(url) {
         var table = $('<table>');
         $.each(files, function(idx, file) {
             var tr = $('<tr>');
+            
+            var a = $('<a>').click(
+                function(e){
+                    insertAtCaret('id_body',
+                        '<img src="' + file.url + '" />'
+                    );
+                }
+            );
             var img = $('<img>').attr({
                 'alt': file.name,
                 'src': file.thumb.url,
                 'width': file.thumb.width,
                 'height': file.thumb.height
             });
-            tr.append($('<td>').append(img));
+            a.append(img);
+            
+            tr.append($('<td>').append(a));
             tr.append($('<td>').text(file.name));
             table.append(tr);
         });
         this.window.append(table);
     }
 }
+
+function insertAtCaret(areaId,text) { 
+    var txtarea = document.getElementById(areaId);
+    var scrollPos = txtarea.scrollTop;
+    var strPos = 0;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? "ff" : (document.selection ? "ie" : false ) );
+    if (br == "ie") { txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        strPos = range.text.length;
+    } else if (br == "ff") strPos = txtarea.selectionStart;
+    var front = (txtarea.value).substring(0,strPos);
+    var back = (txtarea.value).substring(strPos,txtarea.value.length);
+    txtarea.value=front+text+back;
+    strPos = strPos + text.length;
+    if (br == "ie") { 
+        txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        range.moveStart ('character', strPos);
+        range.moveEnd ('character', 0);
+        range.select();
+    } else if (br == "ff") { 
+        txtarea.selectionStart = strPos;
+        txtarea.selectionEnd = strPos;
+        txtarea.focus();
+    }
+    txtarea.scrollTop = scrollPos;
+} 
