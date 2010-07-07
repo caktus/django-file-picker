6609d78daf39265636a88f1bc038956566995b97
Michael Clemmons <glassresistor@gmail.com>
2010-07-07 16:37:53 -0400

    Moved blog over to file-picker wymeditor widget

--- sample_project/blog/admin.py
+++ sample_project/blog/admin.py
@@ -1,19 +1,28 @@
+from django.db import models
 from django.contrib import admin
 from sample_project.blog.models import Post, Image, File
 from sample_project.blog.forms import PostAdminModelForm
-from pagelets import models as pagelets
-from pagelets import admin as pagelets_admin
+
+import file_picker
 
 class PostAdmin(admin.ModelAdmin):
-    form = PostAdminModelForm
+    formfield_overrides = {
+        models.TextField: {
+            'widget': file_picker.widgets.WYMeditorWidget(pickers={
+                'file': 'blog-file',
+                'image': 'blog-image',
+            })
+        },
+    }
     class Media:
         css = {
             "all": (
                     "css/overlay.css",
                    )
         }
-        js = ("js/jquery-1.4.2.min.js",
-              "js/file-picker.js", "js/jquery.tools.min.js",
+        js = ("js/jquery-1.4.2.min.js", "js/jquery.tools.min.js",
+              "wymeditor/jquery.wymeditor.pack.js",
+              "js/file-picker.js", "js/file-picker-wymeditor.js", 
               "js/ajaxupload.js",)
 admin.site.register(Post, PostAdmin)
 
