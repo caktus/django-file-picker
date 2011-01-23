3ef2a2a6d6902f802e34632e199ce7b8e4041849
Colin Copeland <copelco@caktusgroup.com>
2011-01-23 16:25:24 -0500

    remove old sample project

--- sample_project/article/file_pickers.py
+++
@@ -1,5 +0,0 @@
-from sample_project.article.models import Image
-
-import file_picker
-
-file_picker.site.register(Image, name='article')
--- sample_project/blog/file_pickers.py
+++
@@ -1,6 +0,0 @@
-from sample_project.blog.models import Image, File
-
-import file_picker
-
-file_picker.site.register(Image, name='blog-image')
-file_picker.site.register(File, name='blog-file')
