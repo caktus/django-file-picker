859af93380b5dda2ba65c8abdac19018c7a0b7d9
Michael Clemmons <mike@caktusgroup.com>
2011-04-01 13:04:53 -0400

    added MANIFEST.in and updated setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 from setuptools import setup, find_packages
 
-packages = find_packages()
-packages.remove('sample_project')
 setup(
     name='django-file-picker',
-    version='0.0.0',
+    version='0.5.0',
     author='Caktus Consulting Group',
     author_email='solutions@caktusgroup.com',
-    packages=find_packages(),
+    packages=find_packages(exclude=['sample_project']),
     include_package_data=True,
-    exclude_package_data={
-        '': ['*.sql', '*.pyc'],
-    },
     url='https://github.com/caktus/django-file-picker/',
-    license='LICENSE.txt',
+    license='BSD',
     description='Pluggable file picker',
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
     long_description=open('README.rst').read(),
     install_requires=['sorl-thumbnail==3.2.5',],
     zip_safe=False, # because we're including media that Django needs
