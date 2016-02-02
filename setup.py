07e55110989978e781ee9a34bcd54ae1555dd002
Brian Jinwright <bjinwright@qwigo.com>
2016-02-02 16:45:10 -0500

    Updaated sorl-thumbnail in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'Operating System :: OS Independent',
     ],
     long_description=open('README.rst').read(),
-    install_requires=['sorl-thumbnail==12.2',],
+    install_requires=['sorl-thumbnail==12.3',],
     zip_safe=False, # because we're including media that Django needs
 )
 
